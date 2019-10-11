cb3b093b1ca4f0be17aa0de3f44c4aeeefb0f2e1
wallen <wallen_weel@outlook.com>
2019-10-11 18:58:51 +0800

    chore: tags

--- src/components/ColumnLeft.vue
+++ src/components/ColumnLeft.vue
@@ -2,13 +2,19 @@
   <section class="columnLeft">
     <OwnerArea />
 
-    <nav class="labels">
-      <a href="#" class="label">Label 1</a>
-      <a href="#" class="label">Label 2</a>
-      <a href="#" class="label">Label 3</a>
-      <a href="#" class="label">Label 4</a>
-      <a href="#" class="label">Label 5</a>
-      <a href="#" class="label">Label 6</a>
+    <h5>Tags</h5>
+
+    <nav class="tags">
+      <a
+        href="#"
+        class="tag"
+        v-for="tag of tags"
+        :data-id="tag.id"
+        :style="{ color: `#${tag.color}` }"
+        :key="tag.id"
+      >
+        <span>{{ tag.name }}</span>
+      </a>
     </nav>
   </section>
 </template>
@@ -18,6 +24,11 @@ import OwnerArea from "@/components/OwnerArea.vue";
 
 export default {
   name: "ColumnLeft",
+  computed: {
+    tags() {
+      return this.$store.state.tags;
+    }
+  },
   components: {
     OwnerArea
   }
@@ -37,5 +48,48 @@ export default {
   left: 0;
   position: fixed;
   z-index: 6;
+
+  > h5 {
+    margin-bottom: 0;
+    padding: 0 8px;
+  }
+}
+
+.tags {
+  padding: 6px 8px;
+
+  > a {
+    border-radius: 2px;
+    background-color: currentColor;
+    font-size: 12px;
+    padding: 4px 6px;
+    margin: 4px 2px;
+    display: inline-block;
+    position: relative;
+
+    &::before {
+      content: "";
+      opacity: 0.5;
+      box-shadow: 0 1px 2px 1px currentColor;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      top: 0;
+      display: block;
+    }
+
+    > span {
+      color: white;
+      position: relative;
+    }
+
+    &:hover {
+      &::before {
+        transition: box-shadow 0.25s ease;
+        box-shadow: 0 2px 4px 2px currentColor;
+      }
+    }
+  }
 }
 </style>
